ab9977620c51544d19c6d9c9b1d73ae634fe5411
Ensar <eensar.topuz@gmail.com>
2024-02-09 00:39:52 +0300

    add: continous drawing support

--- Assets/2_Simulation/Scripts/DrawableChunk.cs
+++ Assets/2_Simulation/Scripts/DrawableChunk.cs
@@ -1,8 +1,8 @@
 using System;
+using MyBox;
 using UnityEngine;
 
 // TODO: Separate Rendering And Cell System
-// TODO: Add continuous drawing support for mouse drag
 // TODO: Create Cellular Automata Logic For Simulation
 // TODO: Add more detailed classes for Pixel(like Sand, Water, etc.) and Pens(like Brush, Eraser, etc., they may have size). And separate them from DrawableChunk
 // TODO: Add UI buttons for drawing on canvas (choose pixels, pens, or other functional things such as simulation start, pause, etc.)
@@ -26,6 +26,7 @@ namespace UniSand
         // GridData
         private Pixel[,] _cellularGrid;
         private Color32[] _currentColors;
+        private Vector2Int _lastPenHoldPosition = Vector2Int.zero;
 
         private float _cellScale;
 
@@ -82,14 +83,50 @@ namespace UniSand
                 var y = (int) ((mousePos.y + 0.5f) / _cellScale);
                 x = Mathf.Clamp(x, 0, size - 1);
                 y = Mathf.Clamp(y, 0, size - 1);
-                _cellularGrid[x, y] = pixelTypes[1];
+
+                var currentPos = new Vector2Int(x, y);
+                
+                if (_lastPenHoldPosition == Vector2Int.zero)
+                {
+                    DrawSinglePixel(currentPos);
+                }
+
+                else
+                {
+                    DrawLerpLine(currentPos);
+                }
+                
+                _lastPenHoldPosition = new Vector2Int(x, y);
+                
                 SetCurrentColorsFromGrid();
                 ApplyGridToTexture();
-            }  
+            }
+
+            else
+            {
+                _lastPenHoldPosition = Vector2Int.zero;
+            }
         }
-        
-        
-        
+
+        private void DrawLerpLine(Vector2Int currentPos)
+        {
+            float distance = Vector2Int.Distance(_lastPenHoldPosition, currentPos);
+
+            var currentPosition = _lastPenHoldPosition;
+
+            // Calculate how many times we should interpolate between start_point and end_point based on the amount of time that has passed since the last update
+
+            for (int lerp = 0; lerp <= distance; lerp += 1)
+            {
+                currentPosition = Vector2.Lerp(_lastPenHoldPosition, currentPos, lerp / distance).ToVector2Int();
+                DrawSinglePixel(currentPosition);
+            }
+        }
+
+        private void DrawSinglePixel(Vector2Int currentPos)
+        {
+            _cellularGrid[currentPos.x, currentPos.y] = pixelTypes[1];
+        }
     }
 }
 
