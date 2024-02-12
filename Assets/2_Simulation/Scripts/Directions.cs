7f084d0b684931264e471992bc77fde068edd250
Ensar <eensar.topuz@gmail.com>
2024-02-12 22:24:13 +0300

    add: some functionalities for Directions class for future use

--- Assets/2_Simulation/Scripts/Directions.cs
+++ Assets/2_Simulation/Scripts/Directions.cs
@@ -14,5 +14,24 @@ namespace UniSand.Utils
         public static readonly Vector2Int UpRight = new Vector2Int(1, 1);
         public static readonly Vector2Int DownLeft = new Vector2Int(-1, -1);
         public static readonly Vector2Int DownRight = new Vector2Int(1, -1);
+        
+        public static readonly Vector2Int[] All = {Up, Down, Left, Right, UpLeft, UpRight, DownLeft, DownRight};
+        
+        public static Vector2Int RandomDirection()
+        {
+            return All[Random.Range(0, All.Length)];
+        }
+        
+        public static Vector2Int RandomCardinalDirection()
+        {
+            return All[Random.Range(0, 4)];
+        }
+        
+        public static Vector2Int RandomDiagonalDirection()
+        {
+            return All[Random.Range(4, 8)];
+        }
+        
+
     }
 }
